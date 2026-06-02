92a99013341da0413ecd002ac438ebcdd536f876
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:46 -0500

    local title_h = (opts.no_title and 0) or Style.WindowTitleHeight

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -942,3 +942,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
     win._id_stack_save = #g.id_stack
     insert(g.id_stack, "win:" .. tostring(id))
 
+    local title_h = (opts.no_title and 0) or Style.WindowTitleHeight
