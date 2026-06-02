80af0ba0ef174ccd94fc5b026d06fefcad12feab
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:18 -0500

    if on then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1334,3 +1334,4 @@ local function draw_popup()
 
             local bx, byy, bs = px + 12, iy + ih / 2 - 7, 14
             DesirePro.rrect(3, bx, byy, bs, bs, 3, on and DesirePro.col.active or DesirePro.col.anim_default)
+            if on then
