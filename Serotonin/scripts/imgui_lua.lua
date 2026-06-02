8f8b5b11d2f52ce2f8a0d64b9bfd80da1900ead1
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:36 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2793,3 +2793,4 @@ function ImGui.ColorButton(desc_id, col, w, h)
     if ItemAdd(x, y, w, h) then
         dr_rect_filled(2, x, y, w, h, col, Style.FrameRounding)
         dr_rect(2, x, y, w, h, StyleColor(Col.Border), 1, Style.FrameRounding)
+    end
