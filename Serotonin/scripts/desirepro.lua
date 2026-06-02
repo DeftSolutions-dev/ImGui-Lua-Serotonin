5502d38afe407efc3f16e357300e06491aab9616
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:44 -0500

    local t = DesirePro.settings_t

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1697,3 +1697,4 @@ local function draw_settings()
     DesirePro.settings_t = DesirePro.anim(DesirePro.settings_t, DesirePro.settings_open and 1 or 0, 14)
     if DesirePro.settings_t < 0.01 then return end
     DesirePro._modal_active = true
+    local t = DesirePro.settings_t
