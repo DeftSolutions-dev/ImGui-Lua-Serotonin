11e1f63dd9a084f425781b7493c2e3b1f16a43f7
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:27 -0500

    ImGui.AddRectFilled(2, bx, by + bh + 4, bw, 4, ImGui.RGBA(0, 0, 0, 200), 2)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1467,3 +1467,4 @@ function DesirePro.esp_preview(ctx)
     local hh = bh * hp
     ImGui.AddRectFilled(2, hbx, by + bh - hh, 4, hh,
         ImGui.RGBA(floor(255 * (1 - hp)), floor(150 + 90 * hp), 70, 255), 2)
+    ImGui.AddRectFilled(2, bx, by + bh + 4, bw, 4, ImGui.RGBA(0, 0, 0, 200), 2)
