2bf7e514497f3b9c6dade97d4a46767de65413e4
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:06 -0500

    w = w or get_item_width(total_w)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3288,3 +3288,4 @@ end
 function ImGui.BeginListBox(label, w, h)
     local win = g.current_window; if not win then return false end
     local total_w = win._content_x1 - win.cursor_x
+    w = w or get_item_width(total_w)
