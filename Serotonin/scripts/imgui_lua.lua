4dcd4b115fe04736f8fc147ce27c7c354e17aa3e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:55 -0500

    local x = t.x0

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3107,3 +3107,4 @@ function ImGui.TableSetColumnIndex(col)
     local t = win._table; if not t then return end
     if win.cursor_max_y > t.cur_row_max_y then t.cur_row_max_y = win.cursor_max_y end
     t.cur_col = col
+    local x = t.x0
