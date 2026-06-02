671e817aeae2cbbcff1c0436bd05c2766476839f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:35 -0500

    if #g.draw_layer_stack > 0 then remove(g.draw_layer_stack) end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3718,3 +3718,4 @@ function ImGui.EndTooltip()
     me._br_cmd.w = fw; me._br_cmd.h = fh
     remove(g.window_stack)
     g.current_window = g.window_stack[#g.window_stack]
+    if #g.draw_layer_stack > 0 then remove(g.draw_layer_stack) end
