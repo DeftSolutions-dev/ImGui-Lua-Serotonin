1b7bc9c37bbda936e5a93ba3587254dcc9de5e9d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:37 -0500

    if c2.on then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1597,3 +1597,4 @@ function DesirePro.page(idx, px, py, ph)
         ry = ry + c.total + GAP
 
         local c2 = DesirePro.begin_card("Menu settings:This decsription for child", px, ry, CARD_W, 262, "BOMB_FILL")
+        if c2.on then
