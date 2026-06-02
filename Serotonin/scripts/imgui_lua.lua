e0a9bab9c765507bf82e211d31810f5d5748ffcb
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:25 -0500

    local id = GetID("sel:" .. label)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1629,3 +1629,4 @@ end
 
 function ImGui.Selectable(label, selected, w_override, h_override)
     local win = g.current_window; if not win then return false end
+    local id = GetID("sel:" .. label)
