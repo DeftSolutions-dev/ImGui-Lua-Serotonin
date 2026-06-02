99d4f152558b6834f4fc403b0f36781a98318abc
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:37 -0500

    local x, y = win.cursor_x, win.cursor_y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2809,3 +2809,4 @@ function ImGui.BeginCombo(label, preview, flags)
     local total_w = win._content_x1 - win.cursor_x
     local label_part = (tw_lbl > 0) and (tw_lbl + Style.ItemInnerSpacing.x) or 0
     local combo_w = get_item_width(max(40, total_w - label_part))
+    local x, y = win.cursor_x, win.cursor_y
