95354617926a42281aa60a1e700bdc3ffc7b8629
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:35 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2779,3 +2779,4 @@ function ImGui.LabelText(label, fmt, ...)
     dr_text(2, value, x, y + (frame_h - th) * 0.5, StyleColor(Col.Text))
     dr_text(2, label, x + total_w - label_w, y + (frame_h - th) * 0.5, StyleColor(Col.Text))
     ItemSize(total_w, frame_h)
+end
