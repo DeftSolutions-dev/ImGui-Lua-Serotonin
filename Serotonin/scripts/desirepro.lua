b2964048d63841cf31c9277cd410be10a5d224a4
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:40 -0500

    ImGui.PushClipRect(bx, y + CARD_HEADER, w, cs.h + 2)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -797,3 +797,4 @@ function DesirePro.begin_card(name, x, y, w, body_h, icon)
         DesirePro.icon_gradient(2, x + 28, y + 28, icon, 24, 1)
     end
 
+    ImGui.PushClipRect(bx, y + CARD_HEADER, w, cs.h + 2)
