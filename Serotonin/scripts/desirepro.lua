d2c2207013906875ae1d7f9a35cde4b34617aaa3
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:27 -0500

    local hbx = bx - 7

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1462,3 +1462,4 @@ function DesirePro.esp_preview(ctx)
     ImGui.AddRect(2, bx + 1, by + 1, bw - 2, bh - 2, ImGui.RGBA(0, 0, 0, 200), 1, 0)
 
     local hp, armor = 0.72, 0.50
+    local hbx = bx - 7
