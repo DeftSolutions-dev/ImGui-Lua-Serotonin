d1b9378167218be7ed93403728f4d6b212feb56f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:58 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3153,3 +3153,4 @@ function ImGui.TableSetupColumn(label, flags)
                 t.col_widths[i] = max(20, (s.flags.stretch or 1) / max(1, stretch_sum) * stretch_avail)
             end
         end
+    end
