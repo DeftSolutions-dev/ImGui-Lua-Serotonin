36d64456475c1eb6d3890e1702152e90724383c8
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:58 -0500

    if d > days_in_month(y, m) then d = days_in_month(y, m) end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4093,3 +4093,4 @@ function ImGui.DatePicker(label, y, m, d)
     if ImGui.SmallButton(">") then
         m = m + 1
         if m > 12 then m = 1; y = y + 1 end
+        if d > days_in_month(y, m) then d = days_in_month(y, m) end
