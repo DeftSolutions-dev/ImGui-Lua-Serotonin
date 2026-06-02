81bfaeb9d78f4cedb9f80459a63604993b9b2226
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:58 -0500

    win.cursor_y = win.cursor_y + win.line_h + Style.ItemSpacing.y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1149,3 +1149,4 @@ local function ItemSize(w, h)
     win.cursor_max_y = max(win.cursor_max_y, win.cursor_y + win.line_h)
     win.prev_line_h  = win.line_h
     win.cursor_x = win.cursor_start_x + win.indent_x
+    win.cursor_y = win.cursor_y + win.line_h + Style.ItemSpacing.y
