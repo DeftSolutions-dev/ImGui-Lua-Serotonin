e2d01c0382f33307bfae54be6fd0058f4cdfafa6
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:48 -0500

    dr_line(2, cx - 4, cy - 4, cx + 4, cy + 4, lc, 2)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -970,3 +970,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
             local cb_pressed, cb_hov = ButtonBehavior(cx - 7, cy - 7, 14, 14, close_id)
             local lc = cb_hov and StyleColor(Col.ButtonHovered) or StyleColor(Col.Text)
 
+            dr_line(2, cx - 4, cy - 4, cx + 4, cy + 4, lc, 2)
