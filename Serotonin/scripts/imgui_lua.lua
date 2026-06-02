8dac79904803c1fe66ce30308d6ed33d7a9cc427
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:20 -0500

    local id = GetID("imgbtn:" .. tostring(str_id))

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2534,3 +2534,4 @@ end
 
 function ImGui.ImageButton(str_id, tex_id, w, h, tint, bg)
     local win = g.current_window; if not win then return false end
+    local id = GetID("imgbtn:" .. tostring(str_id))
