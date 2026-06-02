329a612c7b606affe82be7bacfd7b3b0d6ced172
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:59 -0500

    local id = GetID("th:" .. label)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3175,3 +3175,4 @@ end
 function ImGui.TableHeader(label)
     local win = g.current_window; if not win then return end
     local t = win._table; if not t then return end
+    local id = GetID("th:" .. label)
