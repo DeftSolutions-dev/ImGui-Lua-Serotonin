b4d9ecee3ca56630ca16d2e6111fa03c1f07b937
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:18 -0500

    local on = sel[i] == true

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1327,3 +1327,4 @@ local function draw_popup()
         for i = 1, n do
             local iy = py + 6 + (i - 1) * ih
             local ih_hover = point_in_rect(mx, my, px, iy, pw, ih)
+            local on = sel[i] == true
