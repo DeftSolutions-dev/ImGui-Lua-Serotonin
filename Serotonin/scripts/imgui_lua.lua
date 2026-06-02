a6ec351c61953586bbe224192ea23b448fb9b2f5
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:30 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3636,3 +3636,4 @@ function ImGui.MenuItem(label, shortcut, selected)
     dr_text(2, label, x + pad_x, y + (h - th) * 0.5, StyleColor(Col.Text))
     if shortcut then
         dr_text(2, shortcut, x + w - pad_x - sw, y + (h - th) * 0.5, StyleColor(Col.TextDisabled))
+    end
