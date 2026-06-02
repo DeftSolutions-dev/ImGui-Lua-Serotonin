2600c7df9aaf0d5f19998b577a6e6960f170247c
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:27 -0500

    ImGui.AddRect(2, bx + 1, by + 1, bw - 2, bh - 2, ImGui.RGBA(0, 0, 0, 200), 1, 0)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1459,3 +1459,4 @@ function DesirePro.esp_preview(ctx)
     local by, bh = py + ph * 0.10, ph * 0.74
     ImGui.AddRect(2, bx - 1, by - 1, bw + 2, bh + 2, ImGui.RGBA(0, 0, 0, 200), 1, 0)
     ImGui.AddRect(2, bx, by, bw, bh, ImGui.RGBA(255, 255, 255, 235), 1, 0)
+    ImGui.AddRect(2, bx + 1, by + 1, bw - 2, bh - 2, ImGui.RGBA(0, 0, 0, 200), 1, 0)
