0f49cdcb0527793e9788230b6d2fa2f88e1cc273
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:48 -0500

    store_set(win, "tabbar_widths_" .. tostring(tb.id), tb.tab_widths)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2021,3 +2021,4 @@ function ImGui.EndTabBar()
             end
         end
         store_set(win, "tabbar_order_"  .. tostring(tb.id), tb.label_order)
+        store_set(win, "tabbar_widths_" .. tostring(tb.id), tb.tab_widths)
