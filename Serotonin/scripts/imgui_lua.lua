abb7d9f7f004e17cb1cb77ff89ad4ed1c22f05b8
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:16 -0500

    if ok_click and clicked then just_clicked = true end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -384,3 +384,4 @@ local function update_io()
 
     local just_clicked = false
     local ok_click, clicked = pcall(mouse.IsClicked, "left")
+    if ok_click and clicked then just_clicked = true end
