2af62f516b53e017d4c7136cc5083752ad11d9c7
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:16 -0500

    win.cursor_max_y   = win.cursor_start_y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2472,3 +2472,4 @@ function ImGui.BeginChild(str_id, w, h, border)
     win.cursor_x       = win.cursor_start_x
     win.cursor_y       = win.cursor_start_y
     win.cursor_max_x   = win.cursor_start_x
+    win.cursor_max_y   = win.cursor_start_y
