406ac99b71758adc8547dd5d3d7cf7fda16a054e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:49 -0500

    DesirePro.pill(2, tmin_x, tmin_y, tw, th, DesirePro.col.anim_default)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -925,3 +925,4 @@ function DesirePro.slider(ctx, label, vmin, vmax, is_int)
     DesirePro.text_mid(2, x + btn / 2, mid_y, "-", "poppins_semibold_18", minus_hit and DesirePro.col.label_active or DesirePro.col.label, 1)
     DesirePro.text_mid(2, x + w - btn / 2, mid_y, "+", "poppins_semibold_18", plus_hit and DesirePro.col.label_active or DesirePro.col.label, 1)
 
+    DesirePro.pill(2, tmin_x, tmin_y, tw, th, DesirePro.col.anim_default)
