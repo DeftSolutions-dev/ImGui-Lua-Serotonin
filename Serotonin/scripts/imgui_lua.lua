af1636d6139099ac9d534d96feb6a53aac0d141f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:11 -0500

    col = col_lerp(col_a, col_b, t)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1372,3 +1372,4 @@ local function button_internal(label, w_override, h_override)
         local col_c = StyleColor(Col.ButtonActive)
         local col
         if t <= 1 then
+            col = col_lerp(col_a, col_b, t)
