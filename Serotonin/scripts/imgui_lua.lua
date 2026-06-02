3cd50c1aa7208af0663707b42025c89ced98f492
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:33 -0500

    local s = get_picker_state(id, r, g_, b)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1750,3 +1750,4 @@ function ImGui.ColorEdit3(label, r, g_, b, flags)
     local visible = ItemAdd(x, y, sq_w + 1, frame_h)
     local pressed, hovered, held = ButtonBehavior(x, y, sq_w, frame_h, id)
     if pressed and not flags.no_picker then
+        local s = get_picker_state(id, r, g_, b)
