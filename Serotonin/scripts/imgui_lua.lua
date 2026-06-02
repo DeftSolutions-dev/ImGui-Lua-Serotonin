4dca1a38388f3f8f429a292519e2283133ae02d8
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:49 -0500

    function ImGui.DragFloat3(label, v, speed, mn, mx, fmt) return vec_widget(label, v, 3, ImGui.DragFloat,  speed, mn, mx, fmt) end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3020,3 +3020,4 @@ local function vec_widget(label, v, n, draw_fn, ...)
     return v
 end
 function ImGui.DragFloat2(label, v, speed, mn, mx, fmt) return vec_widget(label, v, 2, ImGui.DragFloat,  speed, mn, mx, fmt) end
+function ImGui.DragFloat3(label, v, speed, mn, mx, fmt) return vec_widget(label, v, 3, ImGui.DragFloat,  speed, mn, mx, fmt) end
