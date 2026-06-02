2ea7d0ed7fc4cce5e70499a660d7c246c0859581
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:16 -0500

    open_combo = nil

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1305,3 +1305,4 @@ local function draw_popup()
         local b = popup.box
         if rc and can_click and not point_in_rect(mx, my, px, py, pw, ph)
             and not point_in_rect(mx, my, b.x, b.y, b.w, b.h) then
+            open_combo = nil
