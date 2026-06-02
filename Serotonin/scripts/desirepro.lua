06e4f83854b156b3c69a674dcf3b0210b13afc4d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:39 -0500

    if cs.t > 0.01 then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -777,3 +777,4 @@ function DesirePro.begin_card(name, x, y, w, body_h, icon)
     local ptex = load_tex(ROOT .. "/img/grad_pill.png")
     if ptex then
         ImGui.AddImage(2, pill_l, pill_t, pw, ph, ptex, ImGui.ColF(0.13, 0.13, 0.15, 1), 1)
+        if cs.t > 0.01 then
