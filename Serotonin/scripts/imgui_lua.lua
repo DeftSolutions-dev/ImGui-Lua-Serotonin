4fe747d0791789eb40f3606ec7ba103db28c15dd
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:29 -0500

    round(saturate((col.a or 1) * (alpha or 1)) * 255)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -636,3 +636,4 @@ end
 local function to_image_tint(col, alpha)
 
     return Color3_fromRGB(round(saturate(col.r) * 255), round(saturate(col.g) * 255), round(saturate(col.b) * 255)),
+           round(saturate((col.a or 1) * (alpha or 1)) * 255)
