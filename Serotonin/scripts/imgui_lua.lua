7719806ba8a0ea4d0c7213a4b1b558147580ea8d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:43 -0500

    record_item(id, x, y, hk_w + label_part, frame_h, hovered, false, active)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2910,3 +2910,4 @@ function ImGui.Hotkey(label, key)
     if tw_lbl > 0 then
         dr_text(2, label, x + hk_w + Style.ItemInnerSpacing.x, y + (frame_h - th) * 0.5, StyleColor(Col.Text))
     end
+    record_item(id, x, y, hk_w + label_part, frame_h, hovered, false, active)
