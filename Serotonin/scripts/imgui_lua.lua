7584f47792357a7c7d67da35b54004ea9070ad32
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:59 -0500

    win.cursor_y = win.cursor_y + win.prev_line_h + Style.ItemSpacing.y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1167,3 +1167,4 @@ local function auto_wrap(w)
     local win = g.current_window
     if not win then return end
     if win.cursor_x + w > win._content_x1 and win.cursor_x > win.cursor_start_x + win.indent_x then
+        win.cursor_y = win.cursor_y + win.prev_line_h + Style.ItemSpacing.y
