43e23f93976e69814d8e613fafab8a836b4f65d3
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:56 -0500

    record_item(id, x, y, total_w, h, hov, pressed, false)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2143,3 +2143,4 @@ function ImGui.TreeNode(label)
         dr_triangle_filled(2, cx - 2, cy - 4, cx + 4, cy, cx - 2, cy + 4, arr)
     end
     dr_text(2, label, x + 16, y + (h - th) * 0.5, StyleColor(Col.Text))
+    record_item(id, x, y, total_w, h, hov, pressed, false)
