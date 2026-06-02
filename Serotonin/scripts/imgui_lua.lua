8813fc2c54af22edbf01eaff84b697b6a81d9867
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:47 -0500

    win.cursor_y = tb.bottom_y + 4

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1997,3 +1997,4 @@ function ImGui.BeginTabItem(label, p_open)
     local sel = false
     if is_selected and not closed_now then
         win.cursor_x = win.cursor_start_x + win.indent_x
+        win.cursor_y = tb.bottom_y + 4
