210c16cfcd5b9d21576f21eff42101ca63327340
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:23 -0500

    elseif point_in_rect(mx, my, sv_x, alpha_y, sv_w, alpha_h) then color_drag = "alpha" end

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1411,3 +1411,4 @@ local function draw_popup()
         if rc then
             if point_in_rect(mx, my, sv_x, sv_y, sv_w, sv_h) then color_drag = "sv"
             elseif point_in_rect(mx, my, hue_x, sv_y, hue_w, sv_h) then color_drag = "hue"
+            elseif point_in_rect(mx, my, sv_x, alpha_y, sv_w, alpha_h) then color_drag = "alpha" end
