1845cc667c7ea76b5217d0f6869919e6fbcf0d96
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:02 -0500

    dr_line(2, x, y, x + w, y, StyleColor(Col.Separator), 1)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1219,3 +1219,4 @@ function ImGui.Separator()
     local x = win.cursor_x
     local y = win.cursor_y + 2
     local w = win._content_x1 - x
+    dr_line(2, x, y, x + w, y, StyleColor(Col.Separator), 1)
