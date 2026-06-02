849ff6ce06f1a000d9e98790719e0ca60a9bdf00
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:12 -0500

    dr_text(2, label, tx, ty, StyleColor(Col.Text))

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1385,3 +1385,4 @@ local function button_internal(label, w_override, h_override)
         end
         local tx = x + (w - tw) * 0.5
         local ty = y + (h - th) * 0.5
+        dr_text(2, label, tx, ty, StyleColor(Col.Text))
