affd164de27467ff6a9910e7857a41eb63e1b8c6
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:25 -0500

    dr_text(2, overlay, x + (plot_w - ow) * 0.5, y + 2, StyleColor(Col.Text))

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2609,3 +2609,4 @@ local function plot_internal(label, values, overlay, scale_min, scale_max, plot_
         end
         if overlay and overlay ~= "" then
             local ow, _ = text_size(overlay)
+            dr_text(2, overlay, x + (plot_w - ow) * 0.5, y + 2, StyleColor(Col.Text))
