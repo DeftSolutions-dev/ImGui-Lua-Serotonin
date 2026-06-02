cb467a4fde2ef12fb73d7c9d320a7ac31289030d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:46 -0500

    desirepro.lua: line 889

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -886,3 +886,4 @@ function DesirePro.slider(ctx, label, vmin, vmax, is_int)
     local th = tmax_y - tmin_y
     local mid_y = (tmin_y + tmax_y) / 2
     local step = is_int and 1 or ((vmax - vmin) / 100)
+
