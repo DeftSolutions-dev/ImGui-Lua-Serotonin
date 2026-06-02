9cbe69f371a6681c9a20142a72a2201c41f4181f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:22 -0500

    imgui_lua.lua: line 1575

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1572,3 +1572,4 @@ local function drag_internal(label, value, speed, vmin, vmax, fmt, is_int)
     local label_part = (tw_lbl > 0) and (tw_lbl + Style.ItemInnerSpacing.x) or 0
     local drag_w = max(40, total_w - label_part)
     local x, y = win.cursor_x, win.cursor_y
+
