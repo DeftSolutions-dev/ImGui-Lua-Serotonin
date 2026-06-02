881f8b4a2b7487244eaed8fa97707bcbb37284af
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:02 -0500

    if g.mouse_left_clicked and not hovered and g.active_id == id then clear_active_id() end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2253,3 +2253,4 @@ function ImGui.InputText(label, value, callback)
     if hovered and g.mouse_left_clicked then
         set_active_id(id, win, { x = x, y = y, w = input_w, h = frame_h })
     end
+    if g.mouse_left_clicked and not hovered and g.active_id == id then clear_active_id() end
