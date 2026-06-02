5d841b80d99d2d3e95afec7982d4418b9ce39a51
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:57 -0500

    local key = "header_" .. tostring(id)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2158,3 +2158,4 @@ end
 function ImGui.CollapsingHeader(label)
     local win = g.current_window; if not win then return false end
     local id = GetID("hdr:" .. label)
+    local key = "header_" .. tostring(id)
