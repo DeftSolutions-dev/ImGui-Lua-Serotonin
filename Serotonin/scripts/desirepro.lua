0e29d6c22414afbc717d852546aa3e0945cb5ff2
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:42 -0500

    local py = (feat and feat.ay or DesirePro._feature_lasty or 0) + 26 - (1 - t) * 14

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1665,3 +1665,4 @@ local function draw_feature()
     local pw = 230
     local ph = DesirePro._feature_h
     local px = (feat and feat.ax or DesirePro._feature_lastx or 0) - pw
+    local py = (feat and feat.ay or DesirePro._feature_lasty or 0) + 26 - (1 - t) * 14
