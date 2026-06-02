0a8a1f89f67af6a94da2ac210893439380272c4e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:40 -0500

    if icon then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -793,3 +793,4 @@ function DesirePro.begin_card(name, x, y, w, body_h, icon)
     if desc ~= "" then
         DesirePro.text(2, x + 55, y + 30, desc, "poppins_medium_15", DesirePro.col.desc, 1)
     end
+    if icon then
