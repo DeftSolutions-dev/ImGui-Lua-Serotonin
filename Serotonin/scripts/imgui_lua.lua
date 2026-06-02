f9f53af505d63a8be925316b32bbd9ff1ca42349
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:54 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2107,3 +2107,4 @@ function ImGui.TreeNodeEx(label, flags)
         dr_triangle_filled(2, cx - 4, cy - 2, cx + 4, cy - 2, cx, cy + 4, mk)
     else
         dr_triangle_filled(2, cx - 2, cy - 4, cx + 4, cy, cx - 2, cy + 4, mk)
+    end
