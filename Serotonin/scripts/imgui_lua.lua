87b5977ef4880ee7cf6ed26aec2274ae35dba235
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:22 -0500

    local x, y = win.cursor_x, win.cursor_y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2558,3 +2558,4 @@ local function plot_internal(label, values, overlay, scale_min, scale_max, plot_
     plot_h = plot_h or 60
     plot_w = plot_w or (win._content_x1 - win.cursor_x - 80)
     auto_wrap(plot_w)
+    local x, y = win.cursor_x, win.cursor_y
