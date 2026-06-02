60a5e00e131f984e3b7d1a00947773671e9c0774
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:57 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4064,3 +4064,4 @@ local function days_in_month(y, m)
     local d = ({ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 })[m]
     if m == 2 and ((y % 4 == 0 and y % 100 ~= 0) or y % 400 == 0) then d = 29 end
     return d
+end
