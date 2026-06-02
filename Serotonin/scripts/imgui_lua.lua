ed0d219c14993a9a64c5b521ce8fce5afb887d3a
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:03 -0500

    local total_w = win._content_x1 - x

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1228,3 +1228,4 @@ function ImGui.SeparatorText(text)
     local tw, th = text_size(text)
     local x = win.cursor_x
     local y = win.cursor_y + math.floor(th * 0.5)
+    local total_w = win._content_x1 - x
