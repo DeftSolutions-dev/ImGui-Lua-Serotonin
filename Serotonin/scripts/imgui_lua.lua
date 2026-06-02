055a94be8bb1b246e0689fcb47a53d499fae2f42
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:05 -0500

    StyleColor(Col.TableBorderLight), 1)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3270,3 +3270,4 @@ function ImGui.EndTable()
                                StyleColor(Col.SeparatorHovered), 0)
             elseif t.borders then
                 dr_line(3, edge_x, t.y0, edge_x, t.cur_row_max_y,
+                        StyleColor(Col.TableBorderLight), 1)
