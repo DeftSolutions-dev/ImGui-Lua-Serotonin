8ea74aef318ae7929436a100e7f63325696cc5e0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:15 -0500

    content_y1     = win._content_y1,

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2447,3 +2447,4 @@ function ImGui.BeginChild(str_id, w, h, border)
         content_x0     = win._content_x0,
         content_y0     = win._content_y0,
         content_x1     = win._content_x1,
+        content_y1     = win._content_y1,
