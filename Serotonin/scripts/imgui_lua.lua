cd4273671d60e12f3875f946e1a095d49bc56811
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:46 -0500

    col = lc, thick = 2 })

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2967,3 +2967,4 @@ function ImGui.BeginPopupModal(name, p_open, flags)
     local cb_pressed, cb_hov = ButtonBehavior(cx - 7, cy - 7, 14, 14, close_id)
     local lc = cb_hov and StyleColor(Col.ButtonHovered) or StyleColor(Col.Text)
     insert(g.draw_layers[4], { kind = "line", x1 = cx - 4, y1 = cy - 4, x2 = cx + 4, y2 = cy + 4,
+                               col = lc, thick = 2 })
