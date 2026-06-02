aec799ed81f304b0dcb7b61e95ad0f91f24283d8
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:36 -0500

    g.last_item_hovered = hovered

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -759,3 +759,4 @@ local function record_item(id, x, y, w, h, hovered, clicked, active)
     g.last_item_id = id
     local r = g.last_item_rect
     r.x, r.y, r.w, r.h = x, y, w, h
+    g.last_item_hovered = hovered
