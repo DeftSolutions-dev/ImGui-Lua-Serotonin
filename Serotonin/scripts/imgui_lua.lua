beb1a1b0c8fb4f26121f3070026f3632615c2a25
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:18 -0500

    local visible = ItemAdd(x, y, slider_w, frame_h)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1494,3 +1494,4 @@ local function slider_internal(label, value, vmin, vmax, fmt, is_int, flags)
     local slider_w = max(40, total_w - label_part)
     local x, y = win.cursor_x, win.cursor_y
 
+    local visible = ItemAdd(x, y, slider_w, frame_h)
