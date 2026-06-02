6533f6f1e7822e61bec46760a1a2b0f984109a04
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:09 -0500

    DesirePro.text_in(2, x0, y, h, DesirePro.translate(label), "poppins_medium_16", DesirePro.col.label, 1)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1207,3 +1207,4 @@ function DesirePro.input(ctx, label, placeholder)
     local h = floor(40 * DesirePro.US)
     local x0, x1 = appear_xy(id, ctx, h)
     local y = ctx.cy
+    DesirePro.text_in(2, x0, y, h, DesirePro.translate(label), "poppins_medium_16", DesirePro.col.label, 1)
