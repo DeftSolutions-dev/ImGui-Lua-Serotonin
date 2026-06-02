46164bfe269dd1098600912c51a308f8838b592f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:41 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3814,3 +3814,4 @@ function ImGui.LoadIniSettings(path)
                         p.w, p.h = tonumber(pw), tonumber(ph)
                     elseif k == "Collapsed" then
                         p.collapsed = (v == "1")
+                    end
