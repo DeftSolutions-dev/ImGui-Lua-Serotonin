63f8389ad9837f5698aaaf26d1212928d3a31aea
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:45 -0500

    if h.cond == "always" or first_use then win.w, win.h = h.w, h.h end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -927,3 +927,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
     end
     if g.next_window_size then
         local h = g.next_window_size
+        if h.cond == "always" or first_use then win.w, win.h = h.w, h.h end
