ef276740eb52fa971520d7c8e70664923d447f6d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:12 -0500

    _inner_x1       = me.x + 1000,

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3383,3 +3383,4 @@ open_popup_window = function(me)
         _content_y1     = me.y + 1000,
         _inner_x0       = me.x,
         _inner_y0       = me.y,
+        _inner_x1       = me.x + 1000,
