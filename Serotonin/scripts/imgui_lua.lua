eaa9743c979dae9908d8c9c335ffbada060bc235
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:46 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -934,3 +934,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
         local h = g.next_window_collapsed
         if h.cond == "always" or first_use then win.collapsed = h.value end
         g.next_window_collapsed = nil
+    end
