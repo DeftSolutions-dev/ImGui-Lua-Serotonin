985fbe957e627a863ed3c026ade9a0001bdc0f22
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:13 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2423,3 +2423,4 @@ render_scrollbar = function(win)
     if active then col = StyleColor(Col.ScrollbarGrabActive)
     elseif hov then col = StyleColor(Col.ScrollbarGrabHovered) end
     dr_rect_filled(3, sb_x, grab_y, sb_w, grab_h, col, Style.ScrollbarRounding)
+end
