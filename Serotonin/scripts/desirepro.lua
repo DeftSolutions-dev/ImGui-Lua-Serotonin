dbdbffb2c086a708e90933c642901b87814d7e54
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:05 -0500

    window_bg     = R(255, 255, 255, 180), bg           = R(245, 245, 245, 200),

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -286,3 +286,4 @@ DesirePro.col_dark = {
 DesirePro.col_light = {
     active        = R(88, 116, 245, 255), dark          = R(228, 58, 72, 255),
     second        = R(240, 240, 240, 200), background   = R(250, 250, 250, 180),
+    window_bg     = R(255, 255, 255, 180), bg           = R(245, 245, 245, 200),
