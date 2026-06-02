86d6933e32c7e24c3c3595ae1956e330da6010d0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:18:00 -0500

    d = idx

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4120,3 +4120,4 @@ function ImGui.DatePicker(label, y, m, d)
                 local btn_id = "##d" .. tostring(idx)
                 if is_sel then ImGui.PushStyleColor(Col.Button, StyleColor(Col.ButtonActive)) end
                 if ImGui.Button(tostring(idx), cell_w, cell_h) then
+                    d = idx
