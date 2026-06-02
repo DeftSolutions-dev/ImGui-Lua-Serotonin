782b9a4d96776b09b16bc7bd3e7e9a195e6489c7
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:49 -0500

    desirepro.lua: line 933

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -930,3 +930,4 @@ function DesirePro.slider(ctx, label, vmin, vmax, is_int)
         local fw = (s.slow < th) and th or s.slow
         DesirePro.grad_rrect(2, tmin_x, tmin_y, fw, th, th / 2, DesirePro.col.dark, DesirePro.col.active)
     end
+
