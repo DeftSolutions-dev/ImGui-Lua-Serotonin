a89f914c9f4f804145e921bf568fc7a8cb418577
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:22 -0500

    local label_part = (tw_lbl > 0) and (tw_lbl + Style.ItemInnerSpacing.x) or 0

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1569,3 +1569,4 @@ local function drag_internal(label, value, speed, vmin, vmax, fmt, is_int)
     local pad = Style.FramePadding
     local frame_h = th + pad.y * 2
     local total_w = win._content_x1 - win.cursor_x
+    local label_part = (tw_lbl > 0) and (tw_lbl + Style.ItemInnerSpacing.x) or 0
