a9a2cf1b24a58845fad697fb7fabe9159bf3a50e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:03 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3231,3 +3231,4 @@ function ImGui.TableGetSortSpec()
     local win = g.current_window; if not win then return -1, 1 end
     local t = win._table; if not t or not t.sortable then return -1, 1 end
     return t.sort_col, t.sort_dir
+end
