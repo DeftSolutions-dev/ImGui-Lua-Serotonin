88870d2a01076e14b9582687e090f9c404f7e7b9
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:18:05 -0500

    local hovered = ItemHoverable(x, y, w, h, id)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4206,3 +4206,4 @@ function ImGui.VSliderFloat(label, w, h, value, vmin, vmax, fmt)
     auto_wrap(w)
     local x, y = win.cursor_x, win.cursor_y
     local visible = ItemAdd(x, y, w, h)
+    local hovered = ItemHoverable(x, y, w, h, id)
