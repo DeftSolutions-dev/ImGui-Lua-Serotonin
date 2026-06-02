e9191442d93ee0881509b0e4d290a07eb0910c06
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:13 -0500

    else

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2414,3 +2414,4 @@ render_scrollbar = function(win)
         if g.mouse_left_down then
             local new_t = clamp((g.mouse_y - sb_y - grab_h * 0.5) / grab_y_range, 0, 1)
             win.scroll_y = new_t * win.scroll_max_y
+        else
