5f44a504c7e41ec316debee7d1c56d4726458572
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:18:33 -0500

    if x1 then draw.Line(x1, y1, x2, y2, col, c.thick, a) end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4620,3 +4620,4 @@ local function flush_layer(layer)
             a = a * am
             if clip then
                 local x1, y1, x2, y2 = clip_line_cs(c.x1, c.y1, c.x2, c.y2, clip)
+                if x1 then draw.Line(x1, y1, x2, y2, col, c.thick, a) end
