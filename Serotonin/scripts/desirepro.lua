f771631af3679be4f37d0e3ebb92905e1fa1cf91
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:04 -0500

    second        = R(22, 22, 24, 255),   background    = R(22, 22, 27, 255),

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -271,3 +271,4 @@ local R = ImGui.RGBA
 
 DesirePro.col_dark = {
     active        = R(88, 116, 245, 255), dark          = R(228, 58, 72, 255),
+    second        = R(22, 22, 24, 255),   background    = R(22, 22, 27, 255),
