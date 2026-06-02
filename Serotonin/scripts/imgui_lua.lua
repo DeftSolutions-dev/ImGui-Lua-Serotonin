531a522636c08d4c15a65aacc67e329700c8e1e0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:13:54 -0500

    local format, sub, len, byte, char = string.format, string.sub, string.len, string.byte, string.char

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -7,3 +7,4 @@ ImGui.VERSION = "0.1.0"
 
 local floor, ceil, abs, min, max, sqrt = math.floor, math.ceil, math.abs, math.min, math.max, math.sqrt
 local sin, cos, pi, huge = math.sin, math.cos, math.pi, math.huge
+local format, sub, len, byte, char = string.format, string.sub, string.len, string.byte, string.char
