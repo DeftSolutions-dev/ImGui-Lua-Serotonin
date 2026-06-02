5ff9b5939736b6995217ba180f00f2764493da8f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:11 -0500

    dr_rect_filled(3, sb_x, sb_y, sb_w, sb_h, StyleColor(Col.ScrollbarBg), Style.ScrollbarRounding)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2386,3 +2386,4 @@ render_scrollbar = function(win)
     local sb_y = win.y + win._title_h + 1
     local sb_h = win.h - win._title_h - 2
 
+    dr_rect_filled(3, sb_x, sb_y, sb_w, sb_h, StyleColor(Col.ScrollbarBg), Style.ScrollbarRounding)
