ab2d7b11dbba2a1722e33377d0df16132bed79de
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:40 -0500

    local p = g._pending_window_settings[current_title]

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3805,3 +3805,4 @@ function ImGui.LoadIniSettings(path)
             elseif current_title then
                 local k, v = line:match("^([%w_]+)=(.*)$")
                 if k and v then
+                    local p = g._pending_window_settings[current_title]
