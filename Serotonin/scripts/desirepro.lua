a6245bd06c467233fb7178acd97dcd3724224489
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:42 -0500

    if cc > 0.01 then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -830,3 +830,4 @@ function DesirePro.checkbox(ctx, label)
     local bxx, byy, bs = floor(x - grow / 2), floor(y - grow / 2), floor(box + grow)
     local cc = s.check; if cc > 1 then cc = 1 elseif cc < 0 then cc = 0 end
     ImGui.AddRectFilled(2, bxx, byy, bs, bs, DesirePro.col.anim_default, r)
+    if cc > 0.01 then
