d2d467a82071c1b8cb5f3a33a5b75e822ab0cd07
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:59 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3170,3 +3170,4 @@ function ImGui.TableHeadersRow()
     local r0_y = win.cursor_y
     dr_rect_filled(2, t.x0, r0_y, table_total_w(t), Style.FontHeight + Style.FramePadding.y * 2,
                    StyleColor(Col.TableHeaderBg), 0)
+end
