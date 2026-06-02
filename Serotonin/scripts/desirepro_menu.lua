341852277456218faf2927f449e3172ccd824a93
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:59 -0500

    "poppins_medium_16", "poppins_medium_15",

--- Serotonin/scripts/desirepro_menu.lua
+++ Serotonin/scripts/desirepro_menu.lua
@@ -13,3 +13,4 @@ local ImGui = DesirePro.ImGui
 DesirePro.preload({
     fonts = {
         "poppins_semibold_18", "poppins_semibold_17", "poppins_medium_18",
+        "poppins_medium_16", "poppins_medium_15",
