25dacfee0b646647ca7858003b51a0c44ce1fa83
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:38 -0500

    cs.t = DesirePro.anim(cs.t, cs.on and 1 or 0, 12)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -766,3 +766,4 @@ function DesirePro.begin_card(name, x, y, w, body_h, icon)
     local pw, ph = 36, 20
     local pill_l, pill_t = floor(x + w - 48), floor(y + CARD_HEADER / 2 - 10)
     if point_in_rect(mx, my, pill_l, pill_t, pw, ph) and clicked() then cs.on = not cs.on end
+    cs.t = DesirePro.anim(cs.t, cs.on and 1 or 0, 12)
