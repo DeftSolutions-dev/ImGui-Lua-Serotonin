771b355ac03fda4cfa2fca4f9c97a0919ea8425e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:32 -0500

    local frame_h = th + Style.FramePadding.y * 2

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1744,3 +1744,4 @@ function ImGui.ColorEdit3(label, r, g_, b, flags)
     local win = g.current_window; if not win then return r, g_, b end
     local id = GetID("col:" .. label)
     local tw_lbl, th = text_size(label)
+    local frame_h = th + Style.FramePadding.y * 2
