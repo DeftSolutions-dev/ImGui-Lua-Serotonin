5952cf7adc760fc95c2e560d816c36206b947b19
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:26 -0500

    if cmd.y1 then cmd.y1 = py + (cmd.y1 - py) * s end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -581,3 +581,4 @@ local function push_cmd(layer, cmd)
         if cmd.w then cmd.w = cmd.w * s end
         if cmd.h then cmd.h = cmd.h * s end
         if cmd.x1 then cmd.x1 = px + (cmd.x1 - px) * s end
+        if cmd.y1 then cmd.y1 = py + (cmd.y1 - py) * s end
