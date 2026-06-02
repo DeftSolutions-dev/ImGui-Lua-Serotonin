18800a055b9fe643e9e6b363c1e0f3d367ac66b0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:30 -0500

    dr_line(2, cx - 2, cy + 4, cx + 4, cy - 4, cm, 2)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3643,3 +3643,4 @@ function ImGui.MenuItem(label, shortcut, selected)
         local cx = x + w - pad_x - 4
         local cy = y + h * 0.5
         dr_line(2, cx - 6, cy, cx - 2, cy + 4, cm, 2)
+        dr_line(2, cx - 2, cy + 4, cx + 4, cy - 4, cm, 2)
