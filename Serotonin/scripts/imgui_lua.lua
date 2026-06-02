d029d884b7b534f5855b2d2b3de31de409e6a580
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:46 -0500

    local cb_pressed, cb_hov = ButtonBehavior(cx - 6, cy - 6, 12, 12, cid)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1986,3 +1986,4 @@ function ImGui.BeginTabItem(label, p_open)
             local cid = GetID("tabclose:" .. label)
             local cx = x + tab_w - 14
             local cy = y + tab_h * 0.5
+            local cb_pressed, cb_hov = ButtonBehavior(cx - 6, cy - 6, 12, 12, cid)
