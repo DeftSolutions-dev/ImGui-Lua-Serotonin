8b6d59d825997735ba956d5b202dc38b47632657
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:50 -0500

    function ImGui.SliderFloat3(label, v, mn, mx, fmt) return vec_widget(label, v, 3, ImGui.SliderFloat, mn, mx, fmt) end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3026,3 +3026,4 @@ function ImGui.DragInt2  (label, v, speed, mn, mx, fmt) return vec_widget(label,
 function ImGui.DragInt3  (label, v, speed, mn, mx, fmt) return vec_widget(label, v, 3, ImGui.DragInt,    speed, mn, mx, fmt) end
 function ImGui.DragInt4  (label, v, speed, mn, mx, fmt) return vec_widget(label, v, 4, ImGui.DragInt,    speed, mn, mx, fmt) end
 function ImGui.SliderFloat2(label, v, mn, mx, fmt) return vec_widget(label, v, 2, ImGui.SliderFloat, mn, mx, fmt) end
+function ImGui.SliderFloat3(label, v, mn, mx, fmt) return vec_widget(label, v, 3, ImGui.SliderFloat, mn, mx, fmt) end
