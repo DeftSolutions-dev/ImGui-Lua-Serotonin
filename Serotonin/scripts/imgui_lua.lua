6114103cad927628a0b25b24aa2d421937bb9b09
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:58 -0500

    dr_triangle_filled(2, cx - 2, cy - 4, cx + 4, cy, cx - 2, cy + 4, arr)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2181,3 +2181,4 @@ function ImGui.CollapsingHeader(label)
     if open then
         dr_triangle_filled(2, cx - 4, cy - 2, cx + 4, cy - 2, cx, cy + 4, arr)
     else
+        dr_triangle_filled(2, cx - 2, cy - 4, cx + 4, cy, cx - 2, cy + 4, arr)
