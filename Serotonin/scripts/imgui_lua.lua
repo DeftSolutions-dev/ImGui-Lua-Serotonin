598a89f942176ede6c153026f4058589ed450d12
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:33 -0500

    dr_rect(2, x, y, sq_w, frame_h, StyleColor(Col.Border), 1, Style.FrameRounding)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1755,3 +1755,4 @@ function ImGui.ColorEdit3(label, r, g_, b, flags)
     end
     if visible and not flags.no_small_preview then
         dr_rect_filled(2, x, y, sq_w, frame_h, COL(r, g_, b, 1), Style.FrameRounding)
+        dr_rect(2, x, y, sq_w, frame_h, StyleColor(Col.Border), 1, Style.FrameRounding)
