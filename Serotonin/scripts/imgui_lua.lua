a676b9caa00d5f741b0f33978bca7ce17f4c18a5
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:13:54 -0500

    local floor, ceil, abs, min, max, sqrt = math.floor, math.ceil, math.abs, math.min, math.max, math.sqrt

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -5,3 +5,4 @@ print("[imgui_menu] script start")
 local ImGui = {}
 ImGui.VERSION = "0.1.0"
 
+local floor, ceil, abs, min, max, sqrt = math.floor, math.ceil, math.abs, math.min, math.max, math.sqrt
