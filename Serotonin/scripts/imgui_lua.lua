6cd13c3cb11644cd0f07753d264c560d5d3afbf1
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:39 -0500

    local r = g.last_item_rect; return r.x, r.y

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -813,3 +813,4 @@ function ImGui.GetTime()           return g.time end
 function ImGui.GetDeltaTime()      return g.dt end
 function ImGui.GetFrameCount()     return g.frame_count end
 function ImGui.GetItemRectMin()
+    local r = g.last_item_rect; return r.x, r.y
