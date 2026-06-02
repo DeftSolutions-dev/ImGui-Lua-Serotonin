5a032de91916bbc37df45261805bdaedeb236bd2
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:10 -0500

    insert(g.draw_layers[4], br_cmd)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3355,3 +3355,4 @@ open_popup_window = function(me)
                      col = StyleColor(Col.Border), thick = Style.PopupBorderSize,
                      rounding = Style.PopupRounding }
     insert(g.draw_layers[4], bg_cmd)
+    insert(g.draw_layers[4], br_cmd)
