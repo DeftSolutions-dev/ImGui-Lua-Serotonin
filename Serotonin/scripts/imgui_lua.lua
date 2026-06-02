ace03df1553ca81ee7630f465df38c2a2036510d
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:34 -0500

    _inner_x1 = me.x + 1000, _inner_y1 = me.y + 1000,

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3698,3 +3698,4 @@ function ImGui.BeginTooltip()
         _content_x1 = me.x + 1000,
         _content_y1 = me.y + 1000,
         _inner_x0 = me.x, _inner_y0 = me.y,
+        _inner_x1 = me.x + 1000, _inner_y1 = me.y + 1000,
