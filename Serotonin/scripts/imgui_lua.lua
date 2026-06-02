d31a429942cd18e95c7f7bddebece5244a70c2cb
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:27 -0500

    elseif flags.height_largest then visible_rows = 20

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1659,3 +1659,4 @@ function ImGui.Combo(label, current_idx, items, flags)
         local visible_rows
         if flags.height_small then visible_rows = 4
         elseif flags.height_large then visible_rows = 12
+        elseif flags.height_largest then visible_rows = 20
