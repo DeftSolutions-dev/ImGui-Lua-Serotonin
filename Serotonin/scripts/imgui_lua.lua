c3d7aac5a11df3bdd40d756c78971da2c1ef7832
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:55 -0500

    state.input = ImGui.InputText("##cons_in_" .. str_id, state.input)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4046,3 +4046,4 @@ function ImGui.Console(str_id, state, on_submit, w, h)
 
     local total_w = ImGui.GetContentRegionAvail()
     ImGui.SetNextItemWidth(total_w - 70)
+    state.input = ImGui.InputText("##cons_in_" .. str_id, state.input)
