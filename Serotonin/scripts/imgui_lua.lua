07f556ef81153efafbf1b7619e99cef7af1f957a
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:25 -0500

    local ui_scale, ui_px, ui_py = 1, 0, 0

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -554,3 +554,4 @@ local draw_alpha_mul = 1
 function ImGui.SetDrawAlpha(a) draw_alpha_mul = a or 1 end
 function ImGui.GetDrawAlpha() return draw_alpha_mul end
 
+local ui_scale, ui_px, ui_py = 1, 0, 0
