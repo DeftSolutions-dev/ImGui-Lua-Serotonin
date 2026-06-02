9b535d2390a0c6e6e07d43b905f37dcacced6277
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:13:54 -0500

    local insert, remove, concat = table.insert, table.remove, table.concat

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -8,3 +8,4 @@ ImGui.VERSION = "0.1.0"
 local floor, ceil, abs, min, max, sqrt = math.floor, math.ceil, math.abs, math.min, math.max, math.sqrt
 local sin, cos, pi, huge = math.sin, math.cos, math.pi, math.huge
 local format, sub, len, byte, char = string.format, string.sub, string.len, string.byte, string.char
+local insert, remove, concat = table.insert, table.remove, table.concat
