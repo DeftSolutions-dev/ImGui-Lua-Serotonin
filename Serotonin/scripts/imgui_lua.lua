834c414f55bf4b1ef741529bfc8760fbc9479c77
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:24 -0500

    if g.popup_stack[i].id == pid then is_open = true; break end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3566,3 +3566,4 @@ function ImGui.BeginMenu(label)
         local pid = fnv1a("popup:" .. str_id)
         local is_open = false
         for i = 1, #g.popup_stack do
+            if g.popup_stack[i].id == pid then is_open = true; break end
