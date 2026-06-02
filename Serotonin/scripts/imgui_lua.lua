1cac12a3f98b25a4524afef3e9fc96464281bfbd
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:45 -0500

    tb.label_order[pos], tb.label_order[pos - 1] =

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1959,3 +1959,4 @@ function ImGui.BeginTabItem(label, p_open)
             local right_lbl = tb.label_order[pos + 1]
 
             if left_lbl and g.mouse_x < x - (tb.tab_widths[left_lbl] or 80) * 0.5 then
+                tb.label_order[pos], tb.label_order[pos - 1] =
