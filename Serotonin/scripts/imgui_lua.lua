23cc41aea942897af834d5a24a7f76429feedd4b
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:17 -0500

    g.mouse_left_clicked  = (not g.mouse_left_down_prev) and g.mouse_left_down

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -396,3 +396,4 @@ local function update_io()
 
     local probe_down = check_mouse_down_now()
     g.mouse_left_down = probe_down or just_clicked
+    g.mouse_left_clicked  = (not g.mouse_left_down_prev) and g.mouse_left_down
