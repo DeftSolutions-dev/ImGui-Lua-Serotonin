d0255fa0e5781f013ffe70a0d69d6ed7931154ac
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:26 -0500

    ImGui.AddRectFilled(2, bx, by, bw, bh, DesirePro.with_alpha(DesirePro.col.active, st.fa), 6)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -597,3 +597,4 @@ local function draw_tab(i, tab, bx, by, bw, bh)
     st.toff = DesirePro.anim(st.toff, selected and 6 or 0, 12)
 
     if st.fa > 0.002 then
+        ImGui.AddRectFilled(2, bx, by, bw, bh, DesirePro.with_alpha(DesirePro.col.active, st.fa), 6)
