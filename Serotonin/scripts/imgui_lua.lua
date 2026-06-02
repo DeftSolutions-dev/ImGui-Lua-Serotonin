adec0f8b62cf4e0699c35fd04ef1f87a2cb1aff7
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:41 -0500

    local hk_w = get_item_width(max(80, total_w - label_part))

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2876,3 +2876,4 @@ function ImGui.Hotkey(label, key)
     local total_w = win._content_x1 - win.cursor_x
     local tw_lbl = text_size(label)
     local label_part = (tw_lbl > 0) and (tw_lbl + Style.ItemInnerSpacing.x) or 0
+    local hk_w = get_item_width(max(80, total_w - label_part))
