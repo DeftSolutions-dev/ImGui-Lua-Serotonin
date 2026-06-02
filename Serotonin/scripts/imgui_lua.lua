94ebd58c33255b49ccb80d8aa7b5a39e58bffec7
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:00 -0500

    win.cursor_x = win.cursor_start_x + offset_x

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1177,3 +1177,4 @@ function ImGui.SameLine(offset_x, spacing)
     local win = g.current_window; if not win then return end
     win.cursor_y = win.cursor_y - win.prev_line_h - Style.ItemSpacing.y
     if offset_x and offset_x > 0 then
+        win.cursor_x = win.cursor_start_x + offset_x
