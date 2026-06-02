360ddcce3e06ad7d7b76c8ee5af5a34e34fbd19e
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:49 -0500

    local vs = is_int and tostring(floor(v)) or string.format("%.2f", v)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -918,3 +918,4 @@ function DesirePro.slider(ctx, label, vmin, vmax, is_int)
     if grab_cx < tmin_x + 8 then grab_cx = tmin_x + 8 elseif grab_cx > tmax_x - 7 then grab_cx = tmax_x - 7 end
 
     DesirePro.text(2, x, y, DesirePro.translate(label), "poppins_medium_15", DesirePro.col.label, 1)
+    local vs = is_int and tostring(floor(v)) or string.format("%.2f", v)
