661b616aa6732fba53070f3106a5eddb71d16b03
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:34 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2765,3 +2765,4 @@ ImGui._IsDisabled = is_disabled
 function ImGui.AlignTextToFramePadding()
     local win = g.current_window; if not win then return end
     win.cursor_y = win.cursor_y + Style.FramePadding.y
+end
