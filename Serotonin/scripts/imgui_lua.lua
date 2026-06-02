9e9e152a6b4e76a42ea96d90aed61cde7508a97c
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:13:54 -0500

    local sin, cos, pi, huge = math.sin, math.cos, math.pi, math.huge

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -6,3 +6,4 @@ local ImGui = {}
 ImGui.VERSION = "0.1.0"
 
 local floor, ceil, abs, min, max, sqrt = math.floor, math.ceil, math.abs, math.min, math.max, math.sqrt
+local sin, cos, pi, huge = math.sin, math.cos, math.pi, math.huge
