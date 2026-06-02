7f0406818423185d9ee2bd9e29abea26394cf5c4
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:49 -0500

    if tb then

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2023,3 +2023,4 @@ function ImGui.EndTabBar()
         store_set(win, "tabbar_order_"  .. tostring(tb.id), tb.label_order)
         store_set(win, "tabbar_widths_" .. tostring(tb.id), tb.tab_widths)
     end
+    if tb then
