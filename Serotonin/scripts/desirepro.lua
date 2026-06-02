9d4e570949a723fe2cd1642d3f0f6c901dd2f26f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:08 -0500

    return a + (b - a) * t

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -329,3 +329,4 @@ local function delta_time() return ImGui.GetDeltaTime() or 0.016 end
 
 function DesirePro.lerp(a, b, t)
     if t > 1 then t = 1 elseif t < 0 then t = 0 end
+    return a + (b - a) * t
