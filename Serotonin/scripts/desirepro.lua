9a113cafc4f47f7b61ed478101e4fbeedc83e14a
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:08 -0500

    desirepro.lua: line 327

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -324,3 +324,4 @@ function DesirePro.accent_at(y)
     local a, d = DesirePro.col.active, DesirePro.col.dark
     return { r = a.r + (d.r - a.r) * t, g = a.g + (d.g - a.g) * t, b = a.b + (d.b - a.b) * t, a = 1 }
 end
+
