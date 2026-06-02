801cb1d1311faf391dcbf4ed6b65f396fe949fad
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:52 -0500

    if pressed and not flags.leaf then

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2077,3 +2077,4 @@ function ImGui.TreeNodeEx(label, flags)
 
     local hov = ItemHoverable(x, y, total_w, h, id)
     local pressed = hov and g.mouse_left_clicked
+    if pressed and not flags.leaf then
