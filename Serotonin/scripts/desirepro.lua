01e12c4a406dc570c9ef143ba5e6d00d39fe0fcc
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:16 -0500

    and not point_in_rect(mx, my, b.x, b.y, b.w, b.h) then

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1304,3 +1304,4 @@ local function draw_popup()
 
         local b = popup.box
         if rc and can_click and not point_in_rect(mx, my, px, py, pw, ph)
+            and not point_in_rect(mx, my, b.x, b.y, b.w, b.h) then
