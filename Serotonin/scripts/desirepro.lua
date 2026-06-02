df201b45c10c12b9530739532f948b351deb8879
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:34 -0500

    DesirePro.text(2, dx, dy, disp, FONT, col, 0.85)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1557,3 +1557,4 @@ function DesirePro.esp_preview(ctx)
         if it.name == drag then
             local dx, dy = mx - (DesirePro._esp_dx or 0), my - (DesirePro._esp_dy or 0)
             DesirePro.text(2, dx + 1, dy + 1, disp, FONT, ImGui.RGBA(0, 0, 0, 200), 0.7)
+            DesirePro.text(2, dx, dy, disp, FONT, col, 0.85)
