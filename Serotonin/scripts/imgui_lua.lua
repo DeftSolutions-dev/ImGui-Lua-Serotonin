62e733d104a5725a07fd268898cca4c5288beeeb
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:36 -0500

    dr_gradient(2, sv_x, sv_y + i * row_h, sv_size, row_h + 1, cL, cR, true)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1803,3 +1803,4 @@ function ImGui.ColorEdit3(label, r, g_, b, flags)
             local v = 1 - (i + 0.5) / sv_rows
             local cL = COL(v,        v,        v,        1)
             local cR = COL(v * hr,   v * hg,   v * hb,   1)
+            dr_gradient(2, sv_x, sv_y + i * row_h, sv_size, row_h + 1, cL, cR, true)
