65eeeac8809defdae10b165938e93a3a8113e636
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:46 -0500

    ImGui.OpenPopup(str_id); return true

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3895,3 +3895,4 @@ function ImGui.OpenPopupOnItemClick(str_id, button)
     if not g.last_item_id or not r then return false end
     if not point_in_rect(g.mouse_x, g.mouse_y, r.x, r.y, r.w, r.h) then return false end
     if button == 1 and g.mouse_right_clicked then
+        ImGui.OpenPopup(str_id); return true
