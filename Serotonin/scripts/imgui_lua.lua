0b3fea0673242e4cac4ab574712683d44056f68f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:17 -0500

    local logarithmic = flags.logarithmic and vmin > 0 and vmax > 0

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1481,3 +1481,4 @@ end
 
 local function slider_internal(label, value, vmin, vmax, fmt, is_int, flags)
     flags = flags or {}
+    local logarithmic = flags.logarithmic and vmin > 0 and vmax > 0
