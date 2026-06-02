507e2f306b9c85b019c2fff1fe12d550ed083315
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:29 -0500

    imgui_lua.lua: line 2678

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2675,3 +2675,4 @@ function ImGui.GetWindowSize()
 end
 function ImGui.GetWindowWidth()  local w = g.current_window; return w and w.w or 0 end
 function ImGui.GetWindowHeight() local w = g.current_window; return w and w.h or 0 end
+
