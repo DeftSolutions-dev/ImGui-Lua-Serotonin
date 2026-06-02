2b7530dd9ba12fea633ea5e9ab45ed055ad7dcb0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:19:39 -0500

    local knob_cy = pill_t + ph / 2

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -783,3 +783,4 @@ function DesirePro.begin_card(name, x, y, w, body_h, icon)
     end
     ImGui.AddRect(2, pill_l, pill_t, pw, ph, DesirePro.col.second, 1.5, ph / 2)
     local knob_cx = pill_l + 10 + (pw - 20) * cs.t
+    local knob_cy = pill_t + ph / 2
