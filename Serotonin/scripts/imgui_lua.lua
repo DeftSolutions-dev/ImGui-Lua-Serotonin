d4b17ee110ee47de27efdebdfcd09b7d01347bf9
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:16:24 -0500

    end

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2590,3 +2590,4 @@ local function plot_internal(label, values, overlay, scale_min, scale_max, plot_
                 for i = 1, count - 1 do
                     dr_line(2, tx(i), ty(values[i]), tx(i + 1), ty(values[i + 1]), col, 1)
                 end
+            end
