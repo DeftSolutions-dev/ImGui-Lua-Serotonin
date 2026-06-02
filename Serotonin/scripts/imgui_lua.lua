298c28d95abf0f97772fbf8f0b3e21a473fb5489
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:48 -0500

    local cb_pressed, cb_hov = ButtonBehavior(cx - 7, cy - 7, 14, 14, close_id)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -967,3 +967,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
             local close_id = fnv1a("close", id)
             local cx = tx + tw - 14
             local cy = ty + th * 0.5
+            local cb_pressed, cb_hov = ButtonBehavior(cx - 7, cy - 7, 14, 14, close_id)
