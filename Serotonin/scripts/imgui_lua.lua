59ebe3e4da9a932ae165dae58bf12b3f3668cee5
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:14:54 -0500

    imgui_lua.lua: line 1080

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1077,3 +1077,4 @@ function ImGui.Begin(title, opts_or_p_open, maybe_opts)
     win._inner_y0      = win.y + title_h
     win._inner_x1      = win.x + win.w
     win._inner_y1      = win.y + effective_h
+
