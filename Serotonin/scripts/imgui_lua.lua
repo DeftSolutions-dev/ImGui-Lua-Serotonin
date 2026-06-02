0ef4019f213b2d2aab4e624e60b95c991cfe435a
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:50 -0500

    win._tabbar = nil

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -2057,3 +2057,4 @@ function ImGui.EndTabBar()
         win.cursor_x = win.cursor_start_x + win.indent_x
         win.cursor_max_y = max(win.cursor_max_y, win.cursor_y)
     end
+    win._tabbar = nil
