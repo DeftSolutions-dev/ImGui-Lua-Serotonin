1c926e8caf13fa3337be1f030f1d153811cc4b44
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:20:49 -0500

    local ox = floor(pvx + (rx - pvx) / s)

--- Serotonin/scripts/desirepro.lua
+++ Serotonin/scripts/desirepro.lua
@@ -1771,3 +1771,4 @@ function DesirePro.menu()
     local ry = DesirePro.box_y and (pvy + (DesirePro.box_y - pvy) * s) or (pvy - box_rh / 2)
     if rx < 0 then rx = 0 elseif rx > sw - box_rw then rx = sw - box_rw end
     if ry < 0 then ry = 0 elseif ry > sh - box_rh then ry = sh - box_rh end
+    local ox = floor(pvx + (rx - pvx) / s)
