ac7fb8f3185b2ecb715fcdd0d754c52dae2fc658
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:15:41 -0500

    }

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -1892,3 +1892,4 @@ function ImGui.BeginTabBar(name, flags)
         label_order = store_get(win, "tabbar_order_"   .. tostring(id), nil) or {},
         tab_widths  = store_get(win, "tabbar_widths_"  .. tostring(id), nil) or {},
         label_seen  = {},
+    }
