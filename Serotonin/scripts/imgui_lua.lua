45e7e3035a4c50c33da51ca7c3e276f96ddd22f0
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:32 -0500

    local txt_col= col_mul_alpha(StyleColor(Col.Text),    a)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -3669,3 +3669,4 @@ function ImGui.BeginTooltip()
 
     local bg_col = col_mul_alpha(StyleColor(Col.PopupBg), a)
     local br_col = col_mul_alpha(StyleColor(Col.Border),  a)
+    local txt_col= col_mul_alpha(StyleColor(Col.Text),    a)
