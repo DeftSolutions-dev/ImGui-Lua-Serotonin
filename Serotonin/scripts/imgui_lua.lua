d82f0309845cf6843f6373a6421674fc1a9c191f
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2026-06-02 10:17:59 -0500

    ImGui.Dummy(cell_w, cell_h)

--- Serotonin/scripts/imgui_lua.lua
+++ Serotonin/scripts/imgui_lua.lua
@@ -4104,3 +4104,4 @@ function ImGui.DatePicker(label, y, m, d)
         if i > 1 then ImGui.SameLine(nil, 2) end
         local x, _ = ImGui.GetCursorScreenPos()
         dr_text(2, name, x + (cell_w - text_size(name)) * 0.5, win.cursor_y, StyleColor(Col.TextDisabled))
+        ImGui.Dummy(cell_w, cell_h)
